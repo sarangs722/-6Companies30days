d13973ede7ff44e3b714cb0af5f91a820e287138
Sarang Srivastava <sarangs722@gmail.com>
2022-01-05 20:29:37 +0530

    Create PrintAnagramsTogether.java

---
+++ Day 1-5 Goldman Sachs/PrintAnagramsTogether.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class PrintAnagramsTogether {
+    public static void main(String[] args) {
+        String[] words = {"act", "god", "cat", "dog", "tac"};
+        Anagrams(words);
+    }
+
+    public List<List<String>> Anagrams(String[] string_list) {
+        // Code here
+        List<List<String>> result = new ArrayList<List<String>>();
+    	List<String> keyList = new ArrayList<>();// just to maintain order
+    	Map<String, List<String>> map = new HashMap<>();
+    	for(String str: string_list) {
+    		char chars[] = str.toCharArray();
+    		Arrays.sort(chars);
+    		String key = String.valueOf(chars);
+    		// if that key present then get that list and append new element to it
+			List<String> strList = new ArrayList<>();
+    		if(map.containsKey(key)) {
+    			strList = map.get(key);
+    		}
+    		// add key to list if not present - to maintain order of insertion
+    		if(!keyList.contains(key)) {
+    			keyList.add(key);
+    		}
+    		// add the word to correct group
+			strList.add(str);
+    		map.put(key, strList);
+    	}
+    	// add list to result from map as per key order
+    	for(String key : keyList) {
+    		result.add(map.get(key));
+    	}
+    	return result;
+    }
+}
